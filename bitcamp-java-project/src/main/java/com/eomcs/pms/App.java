33352ad6556841d23b96883eec391eb3be95d576
LeeYongMin01 <vbn0213@gmail.com>
2020-08-10 18:44:59 +0900

    8월 10일 수업

--- bitcamp-java-project/src/main/java/com/eomcs/pms/App.java
+++ bitcamp-java-project/src/main/java/com/eomcs/pms/App.java
@@ -1,186 +1,57 @@
 package com.eomcs.pms;
 
-import java.sql.Date;
-import java.util.Scanner;
+import com.eomcs.pms.handler.MemberHandler;
+import com.eomcs.pms.handler.ProjectHandler;
+import com.eomcs.pms.handler.TaskHandler;
+import com.eomcs.util.Prompt;
 
-
+// 1) 회원 정보를 저장할 새 메모리 타입을 정의한다
+// 2) 프로젝트 정보를 저장할 새 메모리 타입을 정의한다
+// 3) 작업 정보를 저장할 새 메모리 타입을 정의한다
 public class App {
-
-  static Scanner keyInput = new Scanner(System.in);
-
-  // 회원
-  static final int LENGTH = 100;
-  static int[] no = new int[LENGTH];
-  static String[] name = new String[LENGTH];
-  static String[] email = new String[LENGTH];
-  static String[] password = new String[LENGTH];
-  static String[] photo = new String[LENGTH];
-  static String[] tel = new String[LENGTH];
-  static Date[] now = new Date[LENGTH];
-  static int count = 0;
-
-  // 프로젝트 정보
-  static final int PLENGTH = 100;
-  static int[] pno = new int[PLENGTH];
-  static String[] ptitle = new String[PLENGTH];
-  static String[] pcontent = new String[PLENGTH];
-  static Date[] pstartDate = new Date[PLENGTH];
-  static Date[] pendDate = new Date[PLENGTH];
-  static String[] powner = new String[PLENGTH];
-  static String[] pmembers = new String[PLENGTH];
-  static int pcount =  0;
-
-  static final int TLENGTH = 100;
-  static int[] tno = new int[TLENGTH];
-  static String[] tcontent = new String[TLENGTH];
-  static Date[] tcompleteDate = new Date[TLENGTH];
-  static String[] tproject = new String[TLENGTH];
-  static int[] tstatus = new int[TLENGTH];
-  static String[] tworker = new String[TLENGTH];
-  static int tcount = 0;
 
   public static void main(String[] args) {
 
-
-
-
-//---------------------------------------------------------------------------------------------
-
     loop:
-    while (true) {
+      while (true) {
+        String command = Prompt.inputString("명령> ");
 
-    String command = promptString("명령> ");
+        switch (command) {
+          case "/member/add":
+            MemberHandler.add();
+            break;
+          case "/member/list":
+            MemberHandler.list();
+            break;
+          case "/project/add":
+            ProjectHandler.add();
+            break;
+          case "/project/list":
+            ProjectHandler.list();
+            break;
+          case "/task/add":
+            TaskHandler.add();
+            break;
+          case "/task/list":
+            TaskHandler.list();
+            break;
+          case "quit":
+          case "exit":
+            System.out.println("안녕!");
+            break loop;
+          default:
+            System.out.println("실행할 수 없는 명령입니다.");
+        }
+        System.out.println(); // 이전 명령의 실행을 구분하기 위해 빈 줄 출력
+      }
 
-    switch (command.toLowerCase()) {
-      case "/member/add":
-        addMember(); // addMember()에 따로 빼낸 코드를 실행, 메소드 호출이라 칭한다.
-        break;
-
-      case "/member/list":
-        listMember();
-        break;
-
-      case "/project/add":
-        addProject();
-        break;
-
-      case "/project/list":
-        listProject();
-        break;
-
-      case "/task/add":
-        addTask();
-        break;
-
-      case "/task/list":
-        listTask();
-        break;
-
-      case "exit":
-      case "quit":
-        System.out.println("안녕!");
-        break loop;
-      default:
-          System.out.println("실행할 수 없는 명령입니다.");
-    }
-    System.out.println();
-}
-
-    keyInput.close();
-    System.out.println("종료!");
-    }
-
-  static void addMember() {
-
-    System.out.println("[회원 등록]");
-
-    no[count] = promptInt("번호? ");
-    name[count] = promptString("이름? ");
-    email[count] = promptString("이메일? ");
-    password[count] = promptString("암호? ");
-    photo[count] = promptString("사진? ");
-    tel[count] = promptString("전화? ");
-    now[count] = new Date(System.currentTimeMillis());
-
-    count++;
-  }
-
-  static void listMember() {
-    System.out.println("[회원 목록]");
-
-    for (int i = 0; i < count; i++) {
-      System.out.printf("%d, %s, %s, %s, %s\n",
-          no[i], name[i], email[i], tel[i], now[i].toString());
-    }
-  }
-
-static void addProject() {
-  System.out.println("[프로젝트 등록]");
-
-  pno[pcount] = promptInt("번호? ");
-  ptitle[pcount] = promptString("프로젝트명? " );
-  pcontent[pcount] = promptString("내용? ");
-  pstartDate[pcount] = promptDate("시작일? ");
-  pendDate[pcount] = promptDate("종료일? ");
-  powner[pcount] = promptString("만든이? ");
-  pmembers[pcount] = promptString("팀원? ");
-  pcount++;
-}
-static void listProject() {
-
-  System.out.println("[프로젝트 목록]");
-
-  for (int i = 0; i < pcount; i++) {
-    System.out.printf("%d, %s, %s, %s, %s\n",
-        pno[i], ptitle[i], pstartDate[i], pendDate[i], powner[i]);
-  }
-}
-static void addTask() {
-
-  System.out.println("[작업 등록]");
-  tproject[tcount] = promptString("프로젝트? ");
-  tno[tcount] = promptInt("번호? ");
-  tcontent[tcount] = promptString("내용? ");
-  tcompleteDate[tcount] = promptDate("완료일? ");
-  tstatus[tcount] = promptInt("상태?\n0: 신규\n1: 진행중\n2: 완료\n> ");
-  tworker[tcount] = promptString("담당자? ");
-  tcount++;
-
-}
-
-static void listTask() {
-
-  System.out.println("[작업 목록]");
-
-  for (int i = 0; i < tcount; i++) {
-    String stateLabel = null;
-    switch (tstatus[i]) {
-      case 1:
-        stateLabel = "진행중";
-        break;
-      case 2:
-        stateLabel = "완료";
-        break;
-      default:
-        stateLabel = "신규";
-    }
-    // 번호, 작업명, 마감일, 프로젝트, 상태, 담당자
-    System.out.printf("%d, %s, %s, %s, %s\n", // 출력 형식 지정
-        tno[i], tcontent[i], tcompleteDate[i], stateLabel, tworker[i]);
+    Prompt.close();
   }
 
 
-}
 
-static int promptInt(String title) {
-  return Integer.parseInt(promptString(title));
-  }
 
-static String promptString(String title) {
-  System.out.print(title);
-  return keyInput.nextLine();
-  }
-static Date promptDate(String title) {
-  return Date.valueOf(promptString(title));
-  }
+
+
+
 }
---
+++ bitcamp-java-project/src/main/java/com/eomcs/pms/handler/ProjectHandler.java
@@ -0,0 +1,54 @@
+package com.eomcs.pms.handler;
+
+import java.sql.Date;
+import com.eomcs.util.Prompt;
+
+public class ProjectHandler {
+
+  static class Project {
+    // Project 클래스에 선언하는 변수는
+    // 기존의 회원 데이터 관련 변수나 작업 데이터 관련 변수와 구분되기 때문에
+    // 변수 이름을 다르게 할 필요가 없다.
+    int no;
+    String title;
+    String content;
+    Date startDate;
+    Date endDate;
+    String owner;
+    String members;
+  }
+  static final int LENGTH = 100;
+  static Project[] list = new Project[LENGTH];
+  static int size = 0;
+
+
+  public static void add() {
+    System.out.println("[프로젝트 등록]");
+
+    Project project = new Project();
+    project.no = Prompt.inputInt("번호? ");
+    project.title = Prompt.inputString("프로젝트명? ");
+    project.content = Prompt.inputString("내용? ");
+    project.startDate = Prompt.inputDate("시작일? ");
+    project.endDate = Prompt.inputDate("종료일? ");
+    project.owner = Prompt.inputString("만든이? ");
+    project.members = Prompt.inputString("팀원? ");
+
+    list[size++] = project;
+  }
+
+  public static void list() {
+    System.out.println("[프로젝트 목록]");
+
+    for (int i = 0; i < size; i++) {
+      Project project = list[i];
+      System.out.printf("%d, %s, %s, %s, %s\n",
+          project.no,
+          project.title,
+          project.startDate,
+          project.endDate,
+          project.owner);
+    }
+  }
+
+}

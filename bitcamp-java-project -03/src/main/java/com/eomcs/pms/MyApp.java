d2446228c5e89c5650e9501f556c836399faf0f9
LeeYongMin01 <vbn0213@gmail.com>
2020-08-19 00:06:25 +0900

    8월 18일 자습

---
+++ bitcamp-java-project -03/src/main/java/com/eomcs/pms/MyApp.java
@@ -0,0 +1,44 @@
+package com.eomcs.pms;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class MyApp {
+	public static void main(String[] args) {
+		System.out.println("[회원]");
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.println("번호? ");
+	String no = scanner.nextLine();
+	
+	System.out.println("이름? ");
+	String name = scanner.nextLine();
+	
+	System.out.println("이메일? ");
+	String email = scanner.nextLine();
+	
+	System.out.println("암호? ");
+	String password = scanner.nextLine();
+	
+	System.out.println("사진? ");
+	String photo = scanner.nextLine();
+	
+	System.out.println("전화? ");
+	String tel = scanner.nextLine();
+	
+	// 현재 일시 
+	Date now = new Date(System.currentTimeMillis());
+	
+	scanner.close();
+	
+	System.out.println("-----------------------------------");
+	
+	System.out.println("번호: " + no);
+	System.out.println("이름: " + name);
+	System.out.println("이메일: " + email);
+	System.out.printf("암호: %s\n", password);
+	System.out.printf("사진: %s\n", photo);
+	System.out.printf("전화: %s\n", tel);
+	System.out.printf("가입일 : %s\n", now);
+	}
+}

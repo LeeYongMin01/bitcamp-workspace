af242511127882e4e5c2c0018e6d879f3d91034d
LeeYongMin01 <vbn0213@gmail.com>
2020-09-07 17:57:37 +0900

    9/7

---
+++ bitcamp-java-project -11/src/main/java/com/eomcs/pms/App_a.java
@@ -0,0 +1,55 @@
+package com.eomcs.pms;
+
+import com.eomcs.pms.handler.BoardHandler;
+import com.eomcs.pms.handler.MemberHandler;
+import com.eomcs.pms.handler.ProjectHandler;
+import com.eomcs.pms.handler.TaskHandler;
+import com.eomcs.util.Prompt;
+
+public class App_a {
+
+  public static void main(String[] args) {
+	 loop:
+	  while(true) {
+    	String command = Prompt.inputString("명령> ");
+
+    	switch(command) {
+    	case "/member/add": 
+    		MemberHandler.add();
+    		break;
+    	case "/member/list":
+    		MemberHandler.list();
+    		  break;
+    		  
+    	case "/project/add":
+    		ProjectHandler.add();
+    		break;
+    	case "/project/list":
+    		ProjectHandler.list();
+    		  break;
+    	
+    	case "/task/add":
+    		TaskHandler.add();
+    		break;
+    	case "/task/list":
+    		TaskHandler.list();
+    		  break;
+    		  
+    	case "/board/add":
+    	  BoardHandler.add();
+    	  break;
+    	
+    	  
+    	case "exit":
+    	case "quit":
+    		System.out.println("안녕!");
+    		break loop;
+    	default:
+    		System.out.println("실행할 수 없는 명령입니다.");
+    	}
+    	System.out.println();
+	  }
+  Prompt.close();
+ }
+}
+    
---
+++ bitcamp-java-project -11/src/main/java/com/eomcs/pms/handler/BoardHandler_a.java
@@ -0,0 +1,34 @@
+package com.eomcs.pms.handler;
+
+import com.eomcs.util.Prompt;
+
+public class BoardHandler_a {
+
+  static class Board {
+    
+    // 게시물 데이터
+    int no;
+    String title;
+    String content;
+    String writer;
+  }
+  static final int LENGTH = 100;
+  static Board[] list = new Board[LENGTH];
+  static int size = 0;
+  
+  public static void add() {
+    System.out.println("[게시글 목록]");
+    
+    Board board = new Board();
+    board.no = Prompt.inputInt("번호? ");
+    board.title = Prompt.inputString("제목? ");
+    board.content= Prompt.inputString("내용? ");
+    board.writer = Prompt.inputString("작성자? ");
+    
+    list[size++] = board;
+    
+    System.out.println("게시글을 등록하였습니다.");
+  }
+}
+
+

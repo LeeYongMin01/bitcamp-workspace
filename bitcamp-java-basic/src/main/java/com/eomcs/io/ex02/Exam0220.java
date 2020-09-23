ca4dbbcb8e3906dbdfa3a285868adde88c3b7018
LeeYongMin01 <vbn0213@gmail.com>
2020-09-23 23:57:30 +0900

    9/23

--- bitcamp-java-basic/src/main/java/com/eomcs/io/ex02/Exam0220.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/io/ex02/Exam0220.java
@@ -1,6 +1,14 @@
+// Byte Stream - 바이트 배열 읽기
 package com.eomcs.io.ex02;
+
+import java.io.FileInputStream;
 
 public class Exam0220 {
 
+  public static void main(String[] args) throws Exception {
+    
+    FileInputStream in = new FileInputStream("temp/test1.data");
+    
+  }
 }
 

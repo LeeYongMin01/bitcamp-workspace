01959d71eebf7388de5fe134a1f0500a77bbff3a
LeeYongMin01 <vbn0213@gmail.com>
2020-10-08 18:14:13 +0900

    10/8

--- bitcamp-java-basic/src/main/java/com/eomcs/net/ex11/step01/CalculatorServer.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/net/ex11/step01/CalculatorServer.java
@@ -1,5 +1,5 @@
 // 계산기 서버 만들기 - 1단계: 단순히 클라이언트 요청에 응답하기
-package com.eomcs.net.ex11.v1;
+package com.eomcs.net.ex11.step01;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -17,13 +17,9 @@ public class CalculatorServer {
           BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
           PrintStream out = new PrintStream(socket.getOutputStream());) {
 
-        String[] input = in.readLine().split(" ");
-
-        int a = Integer.parseInt(input[0]);
-        String op = input[1];
-        int b = Integer.parseInt(input[2]);
-
-        out.printf("결과는 %d 입니다.\n", a + b);
+        out.println("계산기 서버에 오신 걸 환영합니다!");
+        out.println("계산식을 입력하세요!");
+        out.println("예) 23 + 7");
         out.flush();
       }
 
